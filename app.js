849c5526e58faf558054a171bd7ca861b8dad3d6
Student <FrontEnd@iMac-Tim.local>
2016-08-06 13:51:44 +0300

    s

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 (function(){
+    
     var app = angular.module('addition', []);
-
     app.controller("EditController", function(){
-       this.product = info;
+        //this.product = info;
+        
+        
+        this.onSubmit = function(){
+            if (this.log == "admin" && this.pass == "admin"){
+                 console.log("wow");
+            };
+               
+        };
+        
     });
     
 })();
